6b5a123b33f218dba240f8569fe277313fd33eda
Julia Benito <julia.benito@babelgroup.com>
2023-08-18 11:37:35 +0200

    start ionic app

---
+++ app_nomacivi/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_nomacivi',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
